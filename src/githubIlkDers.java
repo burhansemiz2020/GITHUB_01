62f31f1206f03b5d8df213198239fe16794681a1
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-03 01:40:02 +0200

    new version

--- src/githubIlkDers.java
+++ src/githubIlkDers.java
@@ -3,5 +3,6 @@ public class githubIlkDers {
         System.out.println("Hello Github");
         System.out.println("Il y a nouvelle tendance" +
                 "Donc il faut suivre ces inclinitations");
+        System.out.println("gitadd gitpush");
     }
 }
