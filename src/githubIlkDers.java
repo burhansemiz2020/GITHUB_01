b4a4ebb5c4328c82732b5fe5f9ebc0e8c79df69a
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-03 01:04:03 +0200

    04032022

--- src/githubIlkDers.java
+++ src/githubIlkDers.java
@@ -1,5 +1,7 @@
 public class githubIlkDers {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("Il y a nouvelle tendance" +
+                "Donc il faut suivre ces inclinitations");
     }
 }
